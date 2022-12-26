bc4f82572cde8d8d0514d8b8d18c9cff5b3048eb
Andrei Karev <an.carev4@gmail.com>
2022-12-26 13:22:09 +0300

    feat: implement `HomePage` component

---
+++ src/pages/HomePage/HomePage.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Desktop from 'components/Desktop';
+import Container from 'components/Container';
+
+const HomePage = () => {
+  return (
+    <Container>
+      <Desktop />
+    </Container>
+  );
+};
+
+export default HomePage;
