babe8f05c5682919260289d04d64c9cfca786f07
Andrei Karev <an.carev4@gmail.com>
2022-12-26 13:21:07 +0300

    feat: implement `Desktop` component

---
+++ src/components/Desktop/Desktop.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import MyPCIcon from 'components/MyPCIcon';
+import Taskbar from 'components/Taskbar';
+import Window from 'components/Window';
+import styles from './Desktop.module.scss';
+import { useAppSelector } from 'hooks/redux';
+import ContextMenu from 'components/ContextMenu';
+import { useContextMenu } from 'hooks/useContextMenu';
+import { contextMenuModel } from 'data/contextMenuModel';
+
+const Desktop = () => {
+  const openedWindows = useAppSelector((state) => state.desktop.openedWindows);
+  const coordinates = useAppSelector((state) => state.contextMenu.coordinates);
+  const isContextMenuOpened = useAppSelector((state) => state.contextMenu.isContextMenuOpened);
+  const menuItems = useAppSelector((state) => state.contextMenu.menuItems);
+
+  const { handleContextMenu, closeContextMenu } = useContextMenu();
+
+  const windows = openedWindows.map(({ window }) => <Window windowData={window} key={window.id} />);
+
+  return (
+    <>
+      <div
+        className={`${styles.desktop} ${styles.wallpaper_3}`}
+        onContextMenu={(e) => handleContextMenu(e, contextMenuModel.desktop)}
+      >
+        <MyPCIcon />
+      </div>
+      {windows}
+      {isContextMenuOpened && (
+        <ContextMenu
+          coordinates={coordinates}
+          closeContextMenu={closeContextMenu}
+          menuItems={menuItems}
+        />
+      )}
+      <Taskbar />
+    </>
+  );
+};
+
+export default Desktop;
