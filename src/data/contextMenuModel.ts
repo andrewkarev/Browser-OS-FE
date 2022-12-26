445172f4814441252a5d1a2527486113ed0d3745
Andrei Karev <an.carev4@gmail.com>
2022-12-26 13:12:55 +0300

    feat: implement ф list of the available context menu operations

---
+++ src/data/contextMenuModel.ts
@@ -0,0 +1,36 @@
+export const contextMenuModel = {
+  desktop: [{ option: 'set background', title: 'background' }],
+  myPC: [
+    { option: 'open root folder', title: 'open' },
+    { option: 'rename PC icon', title: 'rename' },
+  ],
+  window: [
+    { option: 'add file', title: 'add file' },
+    { option: 'add folder', title: 'add folder' },
+  ],
+  directory: [
+    { option: 'open directory', title: 'open' },
+    { option: '', title: 'separator' },
+    { option: 'cut directory', title: 'cut' },
+    { option: 'copy directory', title: 'copy' },
+    { option: '', title: 'separator' },
+    { option: 'delete directory', title: 'delete' },
+    { option: 'rename directory', title: 'rename' },
+  ],
+  file: [
+    { option: 'open file', title: 'open' },
+    { option: '', title: 'separator' },
+    { option: 'cut file', title: 'cut' },
+    { option: 'copy file', title: 'copy' },
+    { option: '', title: 'separator' },
+    { option: 'delete file', title: 'delete' },
+    { option: 'rename file', title: 'rename' },
+  ],
+  topBar: [
+    { option: 'minimize', title: 'minimize' },
+    { option: 'maximize', title: 'maximize' },
+    { option: '', title: 'separator' },
+    { option: 'close', title: 'close' },
+  ],
+  taskBar: [{ option: 'enter full screen', title: 'open' }],
+};
