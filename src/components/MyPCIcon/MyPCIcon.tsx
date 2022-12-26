59df8a37c5d5c35a4bb5111c38fda33de0aa6005
Andrei Karev <an.carev4@gmail.com>
2022-12-26 13:17:47 +0300

    feat: implement `MyPCIcon` component

---
+++ src/components/MyPCIcon/MyPCIcon.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import styles from './MyPCIcon.module.scss';
+import MY_PC_ICON from '../../assets/icons/my-pc.png';
+import { useAppDispatch } from 'hooks/redux';
+import { getItems } from 'store/reducers/thunks';
+import { useContextMenu } from 'hooks/useContextMenu';
+import { contextMenuModel } from 'data/contextMenuModel';
+
+const MyPCIcon = () => {
+  const dispatch = useAppDispatch();
+
+  const { handleContextMenu } = useContextMenu();
+
+  const handleIconClick = () => {
+    dispatch(getItems(''));
+  };
+
+  return (
+    <div className={styles.wrapper}>
+      <div
+        className={styles.content}
+        onDoubleClick={handleIconClick}
+        onContextMenu={(e) => handleContextMenu(e, contextMenuModel.myPC)}
+      >
+        <div className={styles.iconWrapper}>
+          <img src={MY_PC_ICON} alt="My PC icon" />
+        </div>
+        <div className={styles.title}>My PC</div>
+      </div>
+    </div>
+  );
+};
+
+export default MyPCIcon;
