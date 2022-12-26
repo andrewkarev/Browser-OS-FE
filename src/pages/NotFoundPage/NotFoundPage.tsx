6dd6a7014ea6e61f8227766c5bdda6f87188c893
Andrei Karev <an.carev4@gmail.com>
2022-12-26 13:14:28 +0300

    feat: add `NotFoundPage` component

---
+++ src/pages/NotFoundPage/NotFoundPage.tsx
@@ -0,0 +1,12 @@
+import Container from 'components/Container';
+import React from 'react';
+
+const NotFoundPage = () => {
+  return (
+    <Container>
+      <div>NotFoundPage</div>
+    </Container>
+  );
+};
+
+export default NotFoundPage;
