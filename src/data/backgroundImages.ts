91211d7d68424f052b8f1818c6dfb0bc13e4a328
Andrei Karev <an.carev4@gmail.com>
2022-12-28 17:53:24 +0300

    feat: add list of the available wallpapers

---
+++ src/data/backgroundImages.ts
@@ -0,0 +1,27 @@
+import BIG_SUR from '../assets/images/wallpaper-1.jpg';
+import TWILIGHT from '../assets/images/wallpaper-2.jpg';
+import WAVES from '../assets/images/wallpaper-3.jpg';
+import GOLDEN_GATE from '../assets/images/wallpaper-4.jpg';
+
+export const backgroundImages = [
+  {
+    id: 0,
+    title: 'Big Sur',
+    src: BIG_SUR,
+  },
+  {
+    id: 1,
+    title: 'Twilight',
+    src: TWILIGHT,
+  },
+  {
+    id: 2,
+    title: 'Colorful waves',
+    src: WAVES,
+  },
+  {
+    id: 3,
+    title: 'Golden gate',
+    src: GOLDEN_GATE,
+  },
+];
