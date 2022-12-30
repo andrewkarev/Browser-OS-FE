79140d4bdab39eddef643cf283fdb5e519509151
Andrei Karev <an.carev4@gmail.com>
2022-12-30 15:50:40 +0300

    refactor: update `Window` component

--- src/components/Window/Window.tsx
+++ src/components/Window/Window.tsx
@@ -1,9 +1,11 @@
+import FolderNavigation from 'components/FolderNavigation';
 import Item from 'components/Item/';
 import TopBar from 'components/TopBar/';
 import { contextMenuModel } from 'data/contextMenuModel';
 import { useAppDispatch } from 'hooks/redux';
 import { useContextMenu } from 'hooks/useContextMenu';
 import React, { useEffect } from 'react';
+import { setOpenedWindows, updateOpenedWindow } from 'store/reducers/desktopSlice';
 import { setCurrentWindow } from 'store/reducers/windowSlice';
 import { IWindow } from 'types/IWindow';
 import styles from './Window.module.scss';
@@ -21,6 +23,17 @@ const Window: React.FC<WindowProps> = ({ windowData }) => {
     dispatch(setCurrentWindow(windowData));
   }, [dispatch, windowData]);
 
+  const closeWindow = () => {
+    dispatch(setOpenedWindows(windowData.id));
+  };
+
+  const changeWindowSize = () => {
+    const updatedWindow = { ...windowData };
+    updatedWindow.isWindowMaximized = !windowData.isWindowMaximized;
+
+    dispatch(updateOpenedWindow(updatedWindow));
+  };
+
   const items = windowData.items.map((item, i) => {
     return <Item item={item} windowData={windowData} key={`${item.name}-${i}`} />;
   });
@@ -30,7 +43,13 @@ const Window: React.FC<WindowProps> = ({ windowData }) => {
       className={windowData.isWindowMaximized ? styles.windowMaximized : styles.window}
       onContextMenu={(e) => handleContextMenu(e, contextMenuModel.window, null, windowData)}
     >
-      <TopBar window={windowData} />
+      <TopBar
+        title={windowData.folderTitle}
+        closeWindow={closeWindow}
+        changeWindowSize={changeWindowSize}
+      >
+        <FolderNavigation window={windowData} />
+      </TopBar>
       <div className={styles.itemsContainer}>{items}</div>
     </div>
   );
