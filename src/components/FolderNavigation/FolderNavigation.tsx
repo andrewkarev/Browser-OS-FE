bb0e0ccccc8083d411c0bd546010a4814ffd89aa
Andrei Karev <an.carev4@gmail.com>
2022-12-30 15:43:08 +0300

    refactor: extract top bar navigation buttons into separate component

---
+++ src/components/FolderNavigation/FolderNavigation.tsx
@@ -0,0 +1,66 @@
+import React from 'react';
+import styles from './FolderNavigation.module.scss';
+import WindowOperation from 'common/windowOperation';
+import { useAppDispatch } from 'hooks/redux';
+import { IoChevronBackSharp, IoChevronForwardSharp } from 'react-icons/io5';
+import { updateWindow } from 'store/reducers/thunks';
+import { IWindow } from 'types/IWindow';
+
+interface FolderNavigationProps {
+  window: IWindow;
+}
+
+const FolderNavigation: React.FC<FolderNavigationProps> = ({ window }) => {
+  const dispatch = useAppDispatch();
+
+  const handleBackwardBtnClick = () => {
+    if (!window) return;
+
+    const currentPathIdx = window.history.indexOf(window.currentPath);
+    const previousPath = currentPathIdx ? window.history[currentPathIdx - 1] : '';
+
+    dispatch(
+      updateWindow({
+        itemPath: previousPath,
+        windowId: window.id,
+        operation: WindowOperation.move,
+      })
+    );
+  };
+
+  const handleForwardBtnClick = () => {
+    if (!window) return;
+
+    const currentPathIdx = window.history.indexOf(window.currentPath);
+    const isLast = currentPathIdx === window.history.length - 1;
+    const nextPathId = isLast ? currentPathIdx : currentPathIdx + 1;
+    const nextPath = window.history[nextPathId];
+
+    dispatch(
+      updateWindow({ itemPath: nextPath, windowId: window.id, operation: WindowOperation.move })
+    );
+  };
+
+  return (
+    <div>
+      <button
+        className={styles.backward}
+        disabled={!window?.currentPath}
+        onClick={handleBackwardBtnClick}
+        type="button"
+      >
+        <IoChevronBackSharp className={styles.icon} />
+      </button>
+      <button
+        className={styles.forward}
+        disabled={!window?.history.length || window?.currentPath === window.history.at(-1)}
+        onClick={handleForwardBtnClick}
+        type="button"
+      >
+        <IoChevronForwardSharp className={styles.icon} />
+      </button>
+    </div>
+  );
+};
+
+export default FolderNavigation;
