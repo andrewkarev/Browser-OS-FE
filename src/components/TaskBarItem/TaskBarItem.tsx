b4042921bb1d7299c6079f2a6b658593d7aeb7c2
Andrei Karev <an.carev4@gmail.com>
2023-01-04 10:42:14 +0300

    feat: implement operations with window through the task bar item

--- src/components/TaskBarItem/TaskBarItem.tsx
+++ src/components/TaskBarItem/TaskBarItem.tsx
@@ -2,9 +2,16 @@ import React from 'react';
 import styles from './TaskBarItem.module.scss';
 import ITaskBarItem from 'types/ITaskBarItem';
 import { useAppDispatch, useAppSelector } from 'hooks/redux';
-import { setActiveWindow } from 'store/reducers/desktopSlice';
+import {
+  setActiveWindow,
+  updateOpenedPlayers,
+  updateOpenedWindow,
+} from 'store/reducers/desktopSlice';
 import { useContextMenu } from 'hooks/useContextMenu';
 import { contextMenuModel } from 'data/contextMenuModel';
+import ContextMenuOptions from 'common/contextMenuOptions';
+import { IWindow } from 'types/IWindow';
+import { IMediaFile } from 'types/IMediaFile';
 
 interface TaskBarItemProps {
   item: ITaskBarItem;
@@ -18,19 +25,45 @@ const TaskBarItem: React.FC<TaskBarItemProps> = ({ item }) => {
 
   const { handleContextMenu } = useContextMenu();
 
-  const handleClick = () => {
-    const activeItem = [...openedWindows, ...openedPlayers].find(
-      (openedItem) => openedItem.id === item.id
-    );
+  const getActiveWindow = () => {
+    return [...openedWindows, ...openedPlayers].find((openedItem) => openedItem.id === item.id);
+  };
 
-    activeItem && dispatch(setActiveWindow(activeItem));
+  const changeWindowVisibility = (window: IWindow | IMediaFile) => {
+    'history' in window
+      ? dispatch(updateOpenedWindow({ id: window.id, operation: ContextMenuOptions.minimize }))
+      : dispatch(updateOpenedPlayers({ id: window.id, operation: ContextMenuOptions.minimize }));
+  };
+
+  const handleClick = () => {
+    const isNonActiveWindow = activeWindow?.id !== item.id;
+    const activeItem = getActiveWindow();
+
+    if (!activeItem) return;
+
+    if (isNonActiveWindow && item.isMinimized) {
+      dispatch(setActiveWindow(activeItem));
+      changeWindowVisibility(activeItem);
+    } else if (isNonActiveWindow) {
+      dispatch(setActiveWindow(activeItem));
+    } else {
+      changeWindowVisibility(activeItem);
+    }
+  };
+
+  const handleContextMenuClick = (e: React.MouseEvent) => {
+    const activeItem = getActiveWindow();
+
+    if (activeItem) dispatch(setActiveWindow(activeItem));
+
+    handleContextMenu(e, contextMenuModel.topBar, null);
   };
 
   return (
     <div
       className={`${styles.taskBarItem} ${activeWindow?.id === item.id ? styles.active : ''}`}
       onClick={handleClick}
-      onContextMenu={(e) => handleContextMenu(e, contextMenuModel.topBar, null)}
+      onContextMenu={(e) => handleContextMenuClick(e)}
     >
       {item.title}
     </div>
