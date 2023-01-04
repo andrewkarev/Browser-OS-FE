427dfbf69c143ad28fa6d8dd42e9dcfa83f03afe
Andrei Karev <an.carev4@gmail.com>
2023-01-04 13:18:28 +0300

    feat: implement `VideoPlayer` component

---
+++ src/components/VideoPlayer/VideoPlayer.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { BASE_URL } from 'common/constants';
+import { IMediaFile } from 'types/IMediaFile';
+import styles from './VideoPlayer.module.scss';
+
+interface VideoPlayerProps {
+  fileData: IMediaFile;
+}
+
+const VideoPlayer: React.FC<VideoPlayerProps> = ({ fileData }) => {
+  return (
+    <video className={styles.videoPlayer} controls autoPlay>
+      <source src={`${BASE_URL}/player?videoPath=${fileData.filePath}`} type="video/mp4" />
+    </video>
+  );
+};
+
+export default VideoPlayer;
