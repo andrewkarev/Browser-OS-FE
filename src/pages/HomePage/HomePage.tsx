c6f3062aa105dbe7965283f96be8664e98b78814
Andrei Karev <an.carev4@gmail.com>
2023-01-05 14:12:30 +0300

    feat: implement mouse move tracking in the the `HomePage` component

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,12 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Desktop from 'components/Desktop';
 import Container from 'components/Container';
+import Spinner from 'components/Spinner';
+import Taskbar from 'components/Taskbar';
+import { useAppSelector } from 'hooks/redux';
 
 const HomePage = () => {
+  //
+  const isPending = useAppSelector((state) => state.desktop.isPending);
+
+  const [mouseCoordinates, setMouseCoordinates] = useState({ x: 0, y: 0 });
+
+  const handleMouseMove = (e: React.MouseEvent) => {
+    setMouseCoordinates({ x: e.clientX, y: e.clientY });
+  };
+
   return (
-    <Container>
-      <Desktop />
-    </Container>
+    <div onMouseMove={(e) => handleMouseMove(e)} style={{ cursor: isPending ? 'none' : 'default' }}>
+      <Container>
+        <Desktop />
+        <Taskbar />
+        <Spinner mouseCoordinates={mouseCoordinates} />
+      </Container>
+    </div>
   );
 };
 
